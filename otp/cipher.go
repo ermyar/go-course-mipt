d76b28297fc86697102a8bca19fd9e02f0c39599
Ermachkov Yaroslav <eryar.school@gmail.com>
2025-04-17 21:44:28 +0300

    add task solutions

--- otp/cipher.go
+++ otp/cipher.go
@@ -6,10 +6,44 @@ import (
 	"io"
 )
 
+type streamCipherReader struct {
+	r    io.Reader
+	prng io.Reader
+}
+
+func (sc streamCipherReader) Read(p []byte) (length int, err error) {
+	length, err = sc.r.Read(p)
+	slice := make([]byte, length)
+	sc.prng.Read(slice)
+	for i := range slice {
+		p[i] ^= slice[i]
+	}
+	return length, err
+}
+
 func NewReader(r io.Reader, prng io.Reader) io.Reader {
-	panic("implement me")
+	sc := streamCipherReader{r, prng}
+	intfc := io.Reader(sc)
+	return intfc
+}
+
+type streamCipherWriter struct {
+	w    io.Writer
+	prng io.Reader
+}
+
+func (sc streamCipherWriter) Write(p []byte) (length int, err error) {
+	slice := make([]byte, len(p))
+	sc.prng.Read(slice)
+	for i := range p {
+		slice[i] ^= p[i]
+	}
+	length, err = sc.w.Write(slice)
+	return length, err
 }
 
 func NewWriter(w io.Writer, prng io.Reader) io.Writer {
-	panic("implement me")
+	sc := streamCipherWriter{w, prng}
+	i := io.Writer(sc)
+	return i
 }
--- retryupdate/update.go
+++ retryupdate/update.go
@@ -2,8 +2,96 @@
 
 package retryupdate
 
-import "gitlab.com/slon/shad-go/retryupdate/kvapi"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/gofrs/uuid"
+	"gitlab.com/slon/shad-go/retryupdate/kvapi"
+)
+
+var (
+	conflictError *kvapi.ConflictError
+	authError     *kvapi.AuthError
+	apiError      *kvapi.APIError
+)
+
+func getRealError(err error) error {
+	if err == nil {
+		return nil
+	}
+	if errors.As(err, &apiError) {
+		return err.(*kvapi.APIError).Unwrap()
+	}
+	return nil
+}
+
+var m map[uuid.UUID]bool
+
+func set(c kvapi.Client, key string, newValue *string, oldVersion *uuid.UUID,
+	updateFn func(oldValue *string) (newValue string, err error)) error {
+	setReq := kvapi.SetRequest{Key: key, Value: *newValue, NewVersion: uuid.Must(uuid.NewV4())}
+	if oldVersion != nil {
+		setReq.OldVersion = *oldVersion
+	}
+	fmt.Println("hello1 ", setReq)
+	_, err := c.Set(&setReq)
+	err1 := getRealError(err)
+	if err1 == nil {
+		return nil
+	} else if errors.As(err1, &authError) {
+		fmt.Println(err)
+		return err
+	} else if errors.Is(err1, kvapi.ErrKeyNotFound) {
+		newValue, err := updateFn(nil)
+		if err != nil {
+			return err
+		}
+		return set(c, key, &newValue, nil, updateFn)
+	} else if errors.As(err1, &conflictError) {
+		tmp := err1.(*kvapi.ConflictError)
+		fmt.Println(tmp)
+		if m[tmp.ExpectedVersion] {
+			return nil
+		}
+
+		return getAndSet(c, key, updateFn)
+	} else {
+		m[setReq.NewVersion] = true
+		return set(c, key, newValue, oldVersion, updateFn)
+	}
+}
+
+func getAndSet(c kvapi.Client, key string, updateFn func(oldValue *string) (newValue string, err error)) error {
+	getResp, err := c.Get(&kvapi.GetRequest{Key: key})
+	err1 := getRealError(err)
+	if err == nil {
+		newValue, err := updateFn(&getResp.Value)
+		if err != nil {
+			return err
+		}
+		set(c, key, &newValue, &getResp.Version, updateFn)
+	} else if errors.Is(err1, kvapi.ErrKeyNotFound) {
+		newValue, err := updateFn(nil)
+		if err != nil {
+			return err
+		}
+		return set(c, key, &newValue, nil, updateFn)
+	} else if errors.As(err1, &authError) {
+		return err
+	} else {
+		return getAndSet(c, key, updateFn)
+	}
+	return nil
+}
 
 func UpdateValue(c kvapi.Client, key string, updateFn func(oldValue *string) (newValue string, err error)) error {
-	panic("implement me")
+	m = make(map[uuid.UUID]bool)
+	defer func() {
+		for u := range m {
+			delete(m, u)
+		}
+	}()
+
+	return getAndSet(c, key, updateFn)
 }
