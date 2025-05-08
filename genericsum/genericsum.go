d542a029fee029b6a3bdcddac0dfa1b81eb22db7
Ermachkov Yaroslav <eryar.school@gmail.com>
2025-05-08 14:34:21 +0300

    task solution

--- genericsum/genericsum.go
+++ genericsum/genericsum.go
@@ -2,26 +2,100 @@
 
 package genericsum
 
-func Min(a, b int) int {
-	panic("implement me")
+import (
+	"math/cmplx"
+	"sync"
+
+	"golang.org/x/exp/constraints"
+)
+
+func Min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
 }
 
-func SortSlice(a []int) {
-	panic("implement me")
+func SortSlice[T constraints.Ordered](a []T) {
+	for i := range len(a) {
+		for j := 1; j+i < len(a); j++ {
+			if a[j-1] > a[j] {
+				a[j-1], a[j] = a[j], a[j-1]
+			}
+		}
+	}
 }
 
-func MapsEqual(a, b map[int]int) bool {
-	panic("implement me")
+func MapsEqual[A, B comparable](a, b map[A]B) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for k, v := range a {
+		if val, exist := b[k]; !exist || val != v {
+			return false
+		}
+	}
+	return true
 }
 
-func SliceContains(s []int, v int) bool {
-	panic("implement me")
+func SliceContains[T comparable](s []T, v T) bool {
+	for _, val := range s {
+		if v == val {
+			return true
+		}
+	}
+	return false
 }
 
-func MergeChans(chs ...<-chan int) <-chan int {
-	panic("implement me")
+func MergeChans[T any](chs ...<-chan T) <-chan T {
+	out := make(chan T)
+	wg := sync.WaitGroup{}
+
+	for _, ch := range chs {
+		wg.Add(1)
+		go func(c <-chan T) {
+			for {
+				select {
+				case val, exist := <-c:
+					if exist {
+						out <- val
+					} else {
+						wg.Done()
+						return
+					}
+				}
+			}
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
 
-func IsHermitianMatrix(m [][]int) bool {
-	panic("implement me")
+func Conj(x any) any {
+	switch v := any(x).(type) {
+	case complex64:
+		return complex64(cmplx.Conj(complex128(v)))
+	case complex128:
+		return cmplx.Conj(v)
+	case int:
+		return v
+	default:
+		return nil
+	}
+}
+
+func IsHermitianMatrix[T comparable](m [][]T) bool {
+	for i, ar := range m {
+		for j := range ar {
+			if m[i][j] != Conj(m[j][i]).(T) {
+				return false
+			}
+		}
+	}
+	return true
 }
